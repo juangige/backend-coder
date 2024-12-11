a25ce90b698d5fa1fe15f90ca1b55f9973ec33d2
Juan <juan.gige95@gmail.com>
2024-12-11 14:31:54 -0300

    deploy1

--- src/utils/args.utils.js
+++ src/utils/args.utils.js
@@ -1,7 +1,7 @@
 import { Command } from "commander";
 const args = new Command();
 
-args.option("-p <port>", "puerto", 8080);
+args.option("-p <port>", "puerto", 10000);
 args.option("--mode <mode>", "modo", "prod");
 
 args.parse();
